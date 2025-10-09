25545f9d84b416989f25cae281ae7f62d6e21d34
Cody.McFarland <codymcfarland@protonmail.com>
2025-10-08 20:35:08 -0400

    save

--- app.js
+++ app.js
@@ -50,13 +50,13 @@ firstModalContent.className = `modal-content`;
 
 const disclaimerText = document.createElement(`p`);
 disclaimerText.className = `disclaimer`;
-disclaimerText.textContent = `Disclaimer:
-This game is intended for educational purposes only. All activities, simulations, and information presented within the game are designed to support learning and skill development. No personal data or user information is collected, stored, or shared by the developer.
-By playing this game, you acknowledge and agree that all progress and data remain local to your device, and that the developer assumes no responsibility for any misuse or interpretation of the educational content provided.`
+disclaimerText.textContent = `DISCLAIMER:
+This game is for educational purposes, helping users learn about common scam techniques. The first task challenges you to use the inspect element tool to find the button to close a pop-up a safe example of scam tactics. No personal data is collected or stored; all progress remains on your device.`
 firstModalContent.appendChild(disclaimerText);
 
 const continueButton = document.createElement(`img`);
 continueButton.src = `https://imgs.search.brave.com/YjzvgfxNbCVnYJua-mCyUMjzXD5qaSk8xTssWmuy830/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/aWNvbnNjb3V0LmNv/bS9pY29uL3ByZW1p/dW0vcG5nLTI1Ni10/aHVtYi9jb250aW51/ZS1pY29uLXN2Zy1k/b3dubG9hZC1wbmct/MTI5OTI2MTAucG5n/P2Y9d2VicCZ3PTEy/OA`;
+continueButton.className = `modal-button`
 continueButton.addEventListener(`click`, () => {
     firstModal.style.display = `none`;
     spinButton.disabled = false; //Turns the Spin Button on once Modal closes
@@ -107,7 +107,7 @@ spinButton.addEventListener(`click`, function() {
 
         setTimeout(() => {
             if (isSpinning) {
-                gameMessage.textContent = `You WIN!!!! But....`;
+                gameMessage.textContent = `BIG WIN!!!!`;
                 outputDiv.style.display = `block`;
                 messageP.innerHTML = `<strong>Warning!!!</strong> Scam sites often use these techniques to trick you into sharing your personal information.`;
             }          
